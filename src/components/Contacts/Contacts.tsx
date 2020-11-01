8a1ba80790fb8843e54c6606ab72bb2d6ad88e85
Artem <rozen19942810@gmail.com>
2020-11-02 01:34:41 +0200

    Add useForm & valid fields

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import s from "./Contacts.module.scss";
 import Title from "../../common/common components/Title/Title";
+import {useForm} from "react-hook-form";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
+
+
+type FormType = {
+    name: string
+    "your email": string
+    text: string
+}
 
 const Contacts = () => {
+
+    const schemaLogin = yup.object().shape({
+        "your email": yup.string().required().email(),
+        name: yup.string().required().min(1)
+    })
+    const {register, handleSubmit, reset, errors} = useForm<FormType>({
+        resolver: yupResolver(schemaLogin)
+    })
+
+    const onSubmit = (data: FormType) => {
+        alert(JSON.stringify(data))
+        reset()
+    }
+    errors["your email"] && console.log(errors)
+
     return (
         <div className={s.contactsBlock}>
             <div className={s.contactsContainer}>
-                <form action="">
+                <form action="" onSubmit={handleSubmit(onSubmit)}>
                     <Title title={"contacts"}/>
                     <div className={s.fields}>
-                        <input type="text" placeholder={"Name"}/>
-                        <input type="text" placeholder={"email"}/>
-                        <textarea placeholder={"message"}/>
+                        <input name="name" ref={register({required: true})} type="text" placeholder={"Name"}/>
+                        {errors.name && <span>{errors.name.message}</span>}
+                        <input name="your email" ref={register({required: true})} type="text" placeholder={"email"}/>
+                        {errors["your email"] && <span>{errors["your email"].message}</span>}
+                        <textarea name="text" ref={register({required: true})} placeholder={"message"}/>
                     </div>
                     <input type="submit" value="send"/>
                 </form>
