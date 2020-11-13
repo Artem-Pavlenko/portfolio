07c42707e5a680995ca2048c2567ad06839395eb
Artem <rozen19942810@gmail.com>
2020-11-13 21:57:05 +0200

    Updates

---
+++ precache-manifest.760e6b0c9aa2b3bf714ce9c40f74b58a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8158d5e0d81c517a78dfe8a7692e0fcd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "46a6dc67cd6c2cebd600",
+    "url": "/portfolio/static/css/main.70bd1c49.chunk.css"
+  },
+  {
+    "revision": "e99e9b5f0eb8e13f684b",
+    "url": "/portfolio/static/js/2.44f5044f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/portfolio/static/js/2.44f5044f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a6dc67cd6c2cebd600",
+    "url": "/portfolio/static/js/main.3743b827.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "5c192f53d51175c8ba26e5cc5761b052",
+    "url": "/portfolio/static/media/29063381_2026003124336080_1469933082483901931_n.5c192f53.jpg"
+  },
+  {
+    "revision": "e7d4b6fa1d85dc5a6290397684d963c0",
+    "url": "/portfolio/static/media/ToDo.e7d4b6fa.jpg"
+  },
+  {
+    "revision": "55338d57e6f6c200bcd23f0af4710a9b",
+    "url": "/portfolio/static/media/css.55338d57.svg"
+  },
+  {
+    "revision": "875a7bd5fef2153cc26adcf37ed177c8",
+    "url": "/portfolio/static/media/emile-perron-xrVDYZRGdw4-unsplash.875a7bd5.jpg"
+  },
+  {
+    "revision": "57d3f125e2df9795fe8af82fcd70dc58",
+    "url": "/portfolio/static/media/facebook.57d3f125.svg"
+  },
+  {
+    "revision": "b8dd0f4356902792a84ec4d4657ec59f",
+    "url": "/portfolio/static/media/gmail.b8dd0f43.svg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/portfolio/static/media/instagram.5b23a837.svg"
+  },
+  {
+    "revision": "05ae4838927d685347360b2c2102d300",
+    "url": "/portfolio/static/media/people2.05ae4838.jpg"
+  },
+  {
+    "revision": "eab76719fd611cab5eecb1eb1427675a",
+    "url": "/portfolio/static/media/react.eab76719.svg"
+  },
+  {
+    "revision": "2dcf8108c2fae5ebebea75e4358ca9f3",
+    "url": "/portfolio/static/media/typescript.2dcf8108.svg"
+  }
+]);
