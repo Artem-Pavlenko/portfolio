ad5406b72bf365eb23ea57a810b6538dead4fc18
Artem <rozen19942810@gmail.com>
2020-11-21 19:18:25 +0200

    add download CV

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,18 +4,27 @@ import instagram from "../../assets/instagram.svg"
 import facebook from "../../assets/facebook.svg"
 import gmail from "../../assets/gmail.svg"
 import SocialLink from "./SocialLink/SocilaLink";
+import {BrowserRouter, Link} from "react-router-dom";
+// @ts-ignore
+import cv from "../../assets/CV/CV_Artem_Pavlenko.pdf"
 
 const Fade = require('react-reveal/Fade')
 const Slide = require('react-reveal/Slide')
 
+
+
 const Footer = () => {
     return (
+        <BrowserRouter>
         <Fade>
             <div className={s.footerBlock}>
                 <div className={s.footerContainer}>
                     <a href="#">
                         <h2>Artem Pavlenko</h2>
                     </a>
+                    <div>
+                        <Link to={require('../../assets/CV/CV_Artem_Pavlenko.pdf')} target={'_blank'} download>download CV</Link>
+                    </div>
                     <div className={s.socialNet}>
                         <SocialLink href={"https://www.instagram.com/artem_ww/"} imgSrc={instagram}/>
                         <SocialLink href={"https://www.facebook.com/pavel.tcs/"} imgSrc={facebook}/>
@@ -25,6 +34,7 @@ const Footer = () => {
                 </div>
             </div>
         </Fade>
+        </BrowserRouter>
     )
 }
 
--- src/components/Remote/Remote.tsx
+++ src/components/Remote/Remote.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import s from "./Remote.module.scss";
 import {Fade} from "react-awesome-reveal";
-
+import {BrowserRouter, Link} from "react-router-dom";
+import cvIcon from "../../assets/CV/cv.svg"
 
 
 const Remote = () => {
     return (
-        <Fade>
-            <div className={s.remoteBlock}>
-                <div className={s.circle}>
-                    <div className={s.text}>
-                        <h2>I'm available for <br/>remote work</h2>
-                    </div>
-                    <div className={s.email}>
-                        <h3><a href="mailto: artemdevakk@gmail.com">ArtemDevAkk@gmail.com</a></h3>
-                        <h4>Tel: +380995202588</h4>
+        <BrowserRouter>
+            <Fade>
+                <div className={s.remoteBlock}>
+                    <div className={s.circle}>
+                        <div className={s.text}>
+                            <h2>I'm available for <br/>remote work</h2>
+                        </div>
+                        <div className={s.email}>
+                            <h3><a href="mailto: artemdevakk@gmail.com">ArtemDevAkk@gmail.com</a></h3>
+                            <h4>Tel: +380995202588</h4>
+                            <div className={s.cv}><Link to={require('../../assets/CV/CV_Artem_Pavlenko.pdf')} target={'_blank'}
+                                       download>
+                                <img src={cvIcon} alt="cv"/>
+                            </Link></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Fade>
+            </Fade>
+        </BrowserRouter>
     )
 }
 
