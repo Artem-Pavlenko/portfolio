0c7a3e6a94dc6418136f1dfd5889da053ea6e232
Artem <rozen19942810@gmail.com>
2021-01-06 23:50:31 +0200

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import './App.scss';
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import MySkills from "./components/Skills/MySkills";
-import MyProjects from "./components/Projects/MyProjects";
-import Remote from "./components/Remote/Remote";
-import Contacts from "./components/Contacts/Contacts";
-import Footer from "./components/Footer/Footer";
+import React from 'react'
+import './App.scss'
+import Header from "./components/Header/Header"
+import Main from "./components/Main/Main"
+import MySkills from "./components/Skills/MySkills"
+import MyProjects from "./components/Projects/MyProjects"
+import Remote from "./components/Remote/Remote"
+import Contacts from "./components/Contacts/Contacts"
+import Footer from "./components/Footer/Footer"
 
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Main/>
-      <MySkills/>
-      <MyProjects/>
-      <Remote/>
-      <Contacts/>
-      <Footer/>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <MySkills/>
+            <MyProjects/>
+            <Remote/>
+            <Contacts/>
+            <Footer/>
+        </div>
+    )
 }
 
-export default App;
+export default App
